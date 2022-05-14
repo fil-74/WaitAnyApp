907201379b1111c0140db648bbbe42980e5b856e
fil-74 <vlad.filipenko.02@gmail.com>
2022-05-14 22:24:52 +0300

    v1.0

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WaitAnyApp
+{
+    class Program
+    {
+
+        static public int countofTasks = 10;
+        static void Main(string[] args)
+        {
+            var tasks = new List<Task<string>>();
+            var taskGenerator = new TaskGenerator();
+            for (int i = 0; i < countofTasks; i++)
+            {
+                var newtask = taskGenerator.createTask($"task -- {i+1}");
+                tasks.Add(newtask);
+            }
+            tasks.ForEach(t => t.Start());
+            var firstTask = Task.WaitAny(tasks.ToArray());
+           
+            var result = "";
+            if (firstTask >= 0)
+            {
+                Task<string> task = tasks[firstTask];
+                result = task.Result;
+            }
+            else
+            {
+                result = "Возникла ошибка вычисления";
+            }
+            Console.WriteLine(result);
+        }
+    }
+    class TaskGenerator
+    {
+        private readonly Random random = new Random();
+        private const int minDelay = 600;
+        private const int maxDelay = 40000;
+
+        public Task<string> createTask(string tag)
+        {
+            var delay = random.Next(minDelay, maxDelay);
+            return getTagWithDelay(tag, delay);
+        }
+
+        private Task<string> getTagWithDelay(string tag, int delay)
+        {
+            Console.WriteLine($"==> {tag} has delay {delay}");
+            return new Task<string>(() =>
+            {
+                Thread.Sleep(delay);
+                Console.WriteLine($"==> {tag} executed with delay {delay}");
+                return tag;
+            });
+        }
+    }
+}
